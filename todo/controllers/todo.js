de4f9ff9fd4d76154159520a9193396933d76471
Adrian Mostowski <adaxelx@o2.pl>
2021-10-20 21:29:34 +0200

    post: add error handling, fill tests

--- todo/controllers/todo.js
+++ todo/controllers/todo.js
@@ -1,22 +1,48 @@
 const TodoModel = require("../models/Todo");
 
+const genErrorObject = (message) => ({
+  message,
+});
+
 const Todo = () => {
   const messages = {
     sendTask: {
-      success: { message: "Utworzono nowe zadanie!" },
+      success: genErrorObject("Utworzono nowe zadanie!"),
+      wrongTypeDescription: genErrorObject(
+        "Nie właściwy typ description - oczekiwany typ: string"
+      ),
+      notUnique: genErrorObject("Istnieje już zadanie z takim opisem!"),
+      required: (field) => genErrorObject(`Pole ${field} jest wymagane!`),
+      toLong: (field, max) =>
+        genErrorObject(
+          `Pole ${field} jest ograniczone do ${max} znaków. Skróć wiadomość!`
+        ),
     },
   };
 
+  // POST request in routes/todo
   const sendTask = async (req, res, next) => {
     const { description } = req.body;
+
     try {
       const response = await TodoModel.create({ description });
       res.status(201);
       res.json(messages.sendTask.success);
     } catch (err) {
-      // tutaj lepsza obsługa błędów by się przydała ale to później
-      res.status(500);
-      res.json({ message: err.message });
+      const { path, properties, kind } = err?.errors?.description || {};
+      if (kind === "required") {
+        res.status(400);
+        res.json(messages.sendTask.required(path));
+      } else if (kind === "maxlength") {
+        res.status(400);
+        res.json(messages.sendTask.toLong(path, properties.maxlength));
+      } else if (err.code === 11000) {
+        res.status(409);
+        res.json(messages.sendTask.notUnique);
+      } else {
+        res.status(500);
+        res.json({ message: err.message || "Nieznany błąd." });
+      }
     }
   };
 
--- todo/routes/todo.test.js
+++ todo/routes/todo.test.js
@@ -5,13 +5,76 @@ const app = require("../app");
 const TodoModel = require("../models/Todo");
 const { messages } = require("../controllers/todo");
 const request = supertest(app);
+const { build, fake } = require("@jackfranklin/test-data-bot");
+const { disconnect } = require("../helpers/database");
+const toDo = build("Todo", {
+  fields: {
+    description: fake((f) => f.lorem.sentence()),
+  },
+});
+
+const createTask = async (task) => {
+  const response = await request.post("/todo").send(task);
+  expect(response.body).toEqual(messages.sendTask.success);
+  expect(response.status).toBe(201);
+};
+
+const exacly100Letters =
+  "A9nYQTzzWzOhszNmohNkyFycbarWVzLwiVB5mWcaDVBg1WSSu4mpQJj5qhKnAN5XUq07YZ6bO4c97SmJTI0oNaXhvUuTXxyTU61H";
 
 describe("Todo", () => {
-  it("should create task", async () => {
-    const response = await request.post("/todo").send({
-      description: "Opis",
+  describe("POST - add task", () => {
+    it("should create task", async () => {
+      await createTask(toDo());
+      const almostToLong = toDo({
+        overrides: { description: exacly100Letters },
+      });
+      await createTask(almostToLong);
     });
-    expect(response.body).toEqual(messages.sendTask.success);
-    expect(response.status).toBe(201);
+
+    it("should return error message if body is empty", async () => {
+      const response = await request.post("/todo").send();
+
+      expect(response.body).toEqual(messages.sendTask.required("description"));
+      expect(response.status).toBe(400);
+    });
+
+    it("should return error message if description is not unique", async () => {
+      const todoObject = toDo();
+      const response1 = await request.post("/todo").send(todoObject);
+      const response2 = await request.post("/todo").send(todoObject);
+
+      expect(response2.body).toEqual(messages.sendTask.notUnique);
+      expect(response2.status).toBe(409);
+    });
+
+    it("should return error message if description is to long (more than 100 characters)", async () => {
+      const todoObject = toDo({
+        overrides: { description: fake((f) => f.lorem.sentence(120)) },
+      });
+      const response = await request.post("/todo").send(todoObject);
+
+      expect(response.body).toEqual(
+        messages.sendTask.toLong("description", 100)
+      );
+      expect(response.status).toBe(400);
+    });
+
+    it("should handle unexpected error (for example not connected db)", async () => {
+      await disconnect();
+      const response = await request.post("/todo").send(toDo());
+
+      expect(response.body.message).toMatchInlineSnapshot(
+        `"MongoClient must be connected to perform this operation"`
+      );
+      expect(response.status).toBe(500);
+    });
+
+    it("should return error message if description is passed as empty string", async () => {
+      const response = await request.post("/todo").send({ description: "" });
+
+      expect(response.body).toEqual(messages.sendTask.required("description"));
+      expect(response.status).toBe(400);
+    });
   });
 });
